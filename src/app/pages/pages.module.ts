37c3abac94d5761ce4c8e90b0ab41e392ace37eb
japc <japc.grafico@gmail.com>
2021-10-21 08:19:49 +0200

    Add main pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { SeriesComponent } from './series/series.component';
+import { LatestComponent } from './latest/latest.component';
+import { SearchComponent } from './search/search.component';
+import { ErrorComponent } from './error/error.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MoviesComponent,
+    SeriesComponent,
+    LatestComponent,
+    SearchComponent,
+    ErrorComponent
   ],
   imports: [
     CommonModule
