bb6b28e2a836fc400357f332300b2f61b51ba885
japc <japc.grafico@gmail.com>
2021-11-17 21:03:28 +0100

    Fix querry params

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -5,19 +5,40 @@ import { MovieResponse, Movie } from '../interfaces/movie';
 import { MediaResponse } from '../interfaces/media';
 import { Config } from '../classes/Config';
 import { Tv } from '../interfaces/tv';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  private params = {
-    api_key : Config.API_KEY,
-    region: 'ES',
-    language: 'es-Es'
-  }
+  // private params = {
+  //   api_key : Config.API_KEY,
+  //   language: "es-ES",
+  //   region: "ES",
+  // }
 
-  constructor(private httpClient: HttpClient) { }
+  // private params: HttpParams;
+  private opts = { params: new HttpParams({
+    fromString: `api_key=${Config.API_KEY}&language=es-ES&region: "ES"`
+  })};;
+
+  constructor(private httpClient: HttpClient) {
+
+    // FIXME Query params no funciona correctamente
+    // Al pasar lo parametros  como Objeto.
+    // this.params = new HttpParams({
+    //   fromObject: {
+    //       api_key : Config.API_KEY,
+    //       language: "es-ES",
+    //       region: "ES",
+    //     }
+    // });
+
+    // this.params.append('api_key', Config.API_KEY);
+    // this.params.append('language', 'es-ES')
+    // this.params.append('region', 'ES')
+   }
 
   getLatestTopRatedMovies(): Observable<MovieResponse> {
     const date = new Date();
@@ -27,19 +48,19 @@ export class MoviesService {
     console.log('date', date)
     console.log('aqui', release_data_lte)
 
-    const url = `${Config.BASE_URL}discover/movie?api_key=${this.params.api_key}&primary_release_date.lte=2021-11-01&region=${this.params.region}&with_release_type=3|4&vote_count.gte=10&sort_by=vote_count.desc&primary_release_date.gte=2021-10-15&include_video=true`
-    return this.httpClient.get<MovieResponse>(url)
+    const url = `${Config.BASE_URL}discover/movie?primary_release_date.lte=2021-11-01&with_release_type=3|4&vote_count.gte=10&sort_by=vote_count.desc&primary_release_date.gte=2021-10-15&include_video=true`
+    return this.httpClient.get<MovieResponse>(url, this.opts)
   }
 
 
   getTrending(): Observable<MediaResponse> {
     const url = Config.BASE_URL + "trending/all/week"
-    return this.httpClient.get<MediaResponse>(url, { params: this.params});
+    return this.httpClient.get<MediaResponse>(url, this.opts);
   }
 
 
-  getMedia(type: string, id: Number): Observable<Movie | Tv> {
+  getMedia(type: string, id: Number): Observable<Movie & Tv> {
     const url = `${Config.BASE_URL}${type}/${id}`;
-    return this.httpClient.get<Movie | Tv>(url, { params: this.params});
+    return this.httpClient.get<Movie & Tv>(url, this.opts);
   }
 }
