be4373751f98af57d0360bb7c39ff82184adef80
japc <japc.grafico@gmail.com>
2021-11-21 21:56:28 +0100

    Search suggestions & improvements in styles

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { debounceTime, map, mapTo } from 'rxjs/operators';
 import { Media } from '../../interfaces/media';
 import { MoviesService } from '../../services/movies.service';
 
@@ -14,11 +15,21 @@ export class SearchComponent implements OnInit {
   term!: string;
   results!: Media[];
   isResults: boolean = false;
+  debouncer: Subject<string> = new Subject;
+  showSuggestions: boolean = false;
+  termSuggestions: string[] = [];
 
 
   constructor(private movieService: MoviesService) { }
 
   ngOnInit(): void {
+    this.debouncer
+    .pipe( debounceTime(300))
+    .subscribe( value => {
+      // console.log('debouncer', value);
+      // if (value) this.suggestions(value);
+      if (value) this.suggestions(value);
+    })
   }
 
   search(): void {
@@ -29,12 +40,41 @@ export class SearchComponent implements OnInit {
       .subscribe( resp => {
         if (resp.length > 0) {
           this.results = resp;
+          this.showSuggestions = false;
         } else {
           this.showAlert();
         }
-        console.log(this.results);
       })
-      // console.log(this.term);
+  }
+
+
+  searchSuggestion(term: string): void {
+    this.term = term;
+    this.showSuggestions = false;
+    this.search();
+  }
+
+
+  suggestions(term: string): void {
+    this.showSuggestions = true;
+    this.term = term;
+    const setResults = new Set<string>();
+
+    this.movieService.findMedia(term)
+      .pipe(
+        map(resp => resp.results.filter(item => item.poster_path))
+      )
+      .subscribe( resp => {
+        resp.forEach(item => {
+          setResults.add((item.title) ? item.title + '': item.name + '')
+        })
+        this.termSuggestions = [...setResults].slice(0,5);
+        console.log(this.termSuggestions);
+      })
+  }
+
+  keyPressed() {
+    this.debouncer.next(this.term);
   }
 
   showAlert() {
