4a2bd8a85424959c25e3121072d907a22fafcc75
japc <japc.grafico@gmail.com>
2021-11-23 21:48:36 +0100

    Feature: Random slidesShow, not working

--- src/app/components/slide-show/slide-show.component.ts
+++ src/app/components/slide-show/slide-show.component.ts
@@ -11,9 +11,9 @@ SwiperCore.use([Navigation]);
   styles: [
   ]
 })
-export class SlideShowComponent implements OnInit {
+export class SlideShowComponent implements OnInit  {
 
-  @Input() movies: Media[] = [];
+  @Input() items: Media[] = [];
 
   imagePath: String = Config.IMAGE_URL;
   videoPath: String = Config.VIDEO_URL;
@@ -21,19 +21,32 @@ export class SlideShowComponent implements OnInit {
   config: SwiperOptions = {
     slidesPerView: 1,
     navigation: true,
+    initialSlide: this.itemRandom(),
     pagination: { clickable: true },
   };
 
   onSwiper(swiper:any) {
     console.log(swiper);
   }
+
   onSlideChange() {
     console.log('slide change');
   }
 
   constructor() {
+
   }
 
   ngOnInit(): void {
+  }
+
+  // ngOnChanges(): void {
+  //   // this.setItemRandom();
+  // }
+
+  // FIXME Pasar el valor random al swipper
+  // para mostrar un item diferente cada vez.
+  private itemRandom(): number {
+    return Math.floor(Math.random() * (this.items.length - 1))
   }
 }
