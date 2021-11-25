f2ca32d9ae7998d64b0c9cc93f7b5c37b2e7572d
japc <japc.grafico@gmail.com>
2021-11-25 17:28:43 +0100

    Fix: Querry interface for keys like querys params api

--- src/app/interfaces/Query.ts
+++ src/app/interfaces/Query.ts
@@ -1,29 +1,33 @@
-import { Genres, SortBy, Language, Region  } from '../classes/Config';
+import { Config, Genres, SortBy, Language, Region } from '../classes/Config';
 
-interface Query {
-  language?:      Language;
-  sort_by?:       SortBy;
-  "vote_count.gte"?:    number;
-  "vote_count.lte"?:    number;
-  with_genres?:  Genres;
-  page?: number;
+// FIXME Es posible keys con . o alguna manera de prescindir de las comillas
+// para declarar las keys. De cara a pasar los valores con HttpParams
+export interface IQuery {
+  "api_key"?: Config.API_KEY;
+  "language"?: Language;
+  "sort_by"?: SortBy;
+  "vote_count.gte"?: number;
+  "vote_count.lte"?: number;
+  "vote_average.gte"?: number;
+  "vote_average.lte"?: number;
+  "with_genres"?: Genres;
+  "page"?: number;
 }
 
-export interface QueryMovie extends Query {
-  primary_release_year?: number;
-  year?:  number;
-  region?:        Region;
-  "primary_release_date.gte"?:   Date;
-  "primary_release_date.lte"?:   Date;
-  "release_date.gte"?:   Date;
-  "release_date.lte"?:   Date;
-
+export interface QueryMovie extends IQuery {
+  "primary_release_year"?: number;
+  "year"?: number;
+  "region"?: Region;
+  "primary_release_date.gte"?: Date;
+  "primary_release_date.lte"?: Date;
+  "release_date.gte"?: Date;
+  "release_date.lte"?: Date;
 }
 
-export interface QueryTv extends Query {
-  first_air_date_year?:  number;
-  "air_date.gte"?:   Date;
-  "air_date.lte"?:   Date;
-  "first_air_date.gte"?:   Date;
-  "first_air_date.lte"?:   Date;
+export interface QueryTv extends IQuery {
+  "first_air_date_year"?: number;
+  "air_date.gte"?: Date;
+  "air_date.lte"?: Date;
+  "first_air_date.gte"?: Date;
+  "first_air_date.lte"?: Date;
 }
