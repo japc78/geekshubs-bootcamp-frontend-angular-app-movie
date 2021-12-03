9cf5122b25e175db1a12f40c03fb982c527c17d2
japc <japc.grafico@gmail.com>
2021-12-03 21:14:56 +0100

    Add gui service & bind classes to change theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { GuiService } from './services/gui.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
+
 export class AppComponent {
+
   title = 'app-movie';
+  isDarkMode!: boolean;
+
+  constructor(private guiService: GuiService) {
+    this.guiService.darkMode.subscribe(value => this.isDarkMode = value);
+  }
 }
