06ddbd51d47d3629f63569091216f96c3b6dbac4
japc <japc.grafico@gmail.com>
2021-12-04 10:23:22 +0100

    Styles dark mode in home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GuiService } from './services/gui.service';
 
 @Component({
@@ -6,12 +6,16 @@ import { GuiService } from './services/gui.service';
   templateUrl: './app.component.html',
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   title = 'app-movie';
   isDarkMode!: boolean;
 
   constructor(private guiService: GuiService) {
     this.guiService.darkMode.subscribe(value => this.isDarkMode = value);
+  }
+  ngOnInit(): void {
+    //
+
   }
 }
